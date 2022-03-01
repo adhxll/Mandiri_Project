b3098a6a676b895c4e17097a32d252bd80ab4c51
Adhella <adhellasbl@gmail.com>
2022-03-01 19:18:18 +0700

    Initial Commit

---
+++ Mandiri_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mandiri_Project
+//
+//  Created by Adhella Subalie on 01/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
